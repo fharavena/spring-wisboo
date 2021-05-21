d93338d08882f4b9433c0101677bd9556cb88475
fharavena <fharavena@gmail.com>
2021-05-21 18:59:55 -0400

    CORS fixed

--- src/main/java/com/wisboo/asignacion/controller/favoritoController.java
+++ src/main/java/com/wisboo/asignacion/controller/favoritoController.java
@@ -18,16 +18,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
+
 @RestController
-@RequestMapping(value = "")
+@CrossOrigin(origins = "*")
 public class favoritoController {
 
     @Autowired
